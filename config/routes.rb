05f98ac2b8ce45e06c8f417f076e312e38eb5ce6
Paul Fell <swellpf@gmail.com>
2020-03-10 15:19:13 -0400

    Built remaining models

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  resources :user_movies
+  resources :genres
+  resources :users
+  resources :comments
   resources :movies
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
