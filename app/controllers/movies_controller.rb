53122c37ae38e38687961ddb97824b63e63f2b1b
Paul Fell <swellpf@gmail.com>
2020-04-01 10:58:38 -0400

    Building out controllers

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,2 +1,48 @@
 class MoviesController < ApplicationController
+
+    def new
+        @movie = Movie.new
+    end
+
+    def index
+        if current_user
+            @movies = Movie.all
+        else
+            redirect_to '/'
+        end
+    end
+    
+    
+    
+    def show
+        @movie = Movie.find_by(id: params[:id])
+    end
+
+    def create
+        @movie = Movie.new(movie_params)
+        if @movie.save
+            redirect_to movie_path(@movie)
+        else
+            render 'new'
+        end
+    end
+
+    def edit
+        @movie = Movie.find_by(id: params[:id])
+    end
+
+    def update
+        @movie = Movie.find_by(id: params[:id])
+        if @movie.update(movie_params)
+            redirect_to movie_path(@movie)
+        else
+            render 'edit'
+        end
+    end
+
+private
+
+    def movie_params
+        params.require(:movie).permit(:name, :genre, :rating)
+    end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
+    has_many :comments
     has_many :movies, through: :user_movies
-    
+
 
 end
